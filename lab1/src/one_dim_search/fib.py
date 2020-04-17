2d69210a591371d6e6a1861f96df33c5bb8f8a6e
Golovin Pavel <gopavel0@gmail.com>
2020-04-17 19:48:06 +0300

    [lab1] fix and visualize one_dim

--- lab1/src/one_dim_search/fib.py
+++ lab1/src/one_dim_search/fib.py
@@ -1,14 +1,17 @@
 from typing import Callable, Iterator, Tuple
 
+fib = [1, 1, 2]  # 3, 5, 8
+
 
 def search(l: float, r: float, *, f: Callable[[float], float], eps: float) -> Iterator[Tuple[float, float]]:
     # precalc fib
-    fib = [1, 1, 2]  # 3, 5, 8
+    global fib
     n = 0
     rel_len = (r - l) / eps
     while fib[n + 2] < rel_len:
-        fib.append(fib[n + 2] + fib[n + 1])
         n += 1
+        if len(fib) <= n + 2:
+            fib.append(fib[n + 1] + fib[n])
     # search
     yield l, r
     #     3   2  3
@@ -18,7 +21,7 @@ def search(l: float, r: float, *, f: Callable[[float], float], eps: float) -> It
     x2 = fib[n + 1] / fib[n + 2] * (r - l)
     f1, f2 = f(x1), f(x2)
     for i in range(1, n + 1):
-        if f1 < f2:
+        if f2 < f1:
             l = x1
             x1 = x2
             x2 = l + fib[n + 1 - i] / fib[n + 2 - i] * (r - l)
--- lab1/src/one_dim_search/golden.py
+++ lab1/src/one_dim_search/golden.py
@@ -6,8 +6,8 @@ def search(l, r, *, f: Callable[[float], float], eps: float) -> Iterator[Tuple[f
     phi = (1 + sqrt(5)) / 2
 
     # optimization to calculate only one f-value in each step
-    x_l = l + (1 - phi) * (r - l)
-    x_r = r - (1 - phi) * (r - l)
+    x_l = l + (2 - phi) * (r - l)
+    x_r = r - (2 - phi) * (r - l)
 
     f_l = f(x_l)
     f_r = f(x_r)
@@ -20,16 +20,15 @@ def search(l, r, *, f: Callable[[float], float], eps: float) -> Iterator[Tuple[f
             x_r = x_l
             f_r = f_l
 
-            x_l = l + (1 - phi) * (r - l)
+            x_l = l + (2 - phi) * (r - l)
             f_l = f(x_l)
         elif f_l > f_r:
             l = x_l
             x_l = x_r
             f_l = f_r
 
-            x_r = r - (1 - phi) * (r - l)
+            x_r = r - (2 - phi) * (r - l)
             f_r = f(x_r)
         else:
             break
         yield l, r
-    yield r + l / 2, r + l / 2
