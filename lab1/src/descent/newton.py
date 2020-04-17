bf01b57d2a3221ce08c87ebd37c429eff8fed6c0
Denis Vaksman <denis.vaksman@corp.vk.com>
2020-04-18 01:40:24 +0300

    [lab1] add newton

---
+++ lab1/src/descent/newton.py
@@ -0,0 +1,49 @@
+from typing import Callable, Iterator
+import numpy as np
+from more_itertools import last
+
+
+def newton_descent_iter(*, f: Callable[[np.ndarray], float],
+                        f_grad: Callable[[np.ndarray], np.ndarray],
+                        f_hess: Callable[[np.ndarray], np.ndarray], eps: float,
+                        start: np.ndarray, cho_mode=False) -> Iterator[np.ndarray]:
+    assert 0 <= 1 - eps < 1
+    cur = start
+    yield cur
+    start_hess = f_hess(start)
+    iter_cnt = 0
+    while True:
+        if iter_cnt and iter_cnt % 100000 == 0:
+            print("newton iter: %d" % iter_cnt)
+            print(cur)
+        grad = f_grad(cur)
+        hess = f_hess(cur)
+        hess_inv = np.linalg.inv(hess)
+
+        if np.linalg.norm(hess) ** 2 <= eps * np.linalg.norm(start_hess) ** 2:
+            return
+
+        cur = cur - np.matmul(grad, hess_inv)
+        yield cur
+        iter_cnt += 1
+
+
+def newton_descent(*args, **kwargs) -> np.array:
+    return last(newton_descent_iter(*args, **kwargs))
+
+
+# f(x,y) = x^2 + y^2
+
+if __name__ == '__main__':
+    def f(p):
+        return p[0] ** 2 + p[1] ** 2
+
+
+    def f_grad(p):
+        x, y = p[0], p[1]
+        return np.array([2 * x, 2 * y])
+
+    def f_hess(p):
+        return np.array([[2, 0], [0, 2]])
+
+    print(newton_descent(f=f, f_grad=f_grad, f_hess=f_hess, eps=1e-5, start=np.array([3, 2])))
