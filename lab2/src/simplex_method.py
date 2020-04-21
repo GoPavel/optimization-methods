728de097b74765b6912b49ae69a0871720fa8583
Golovin Pavel <gopavel0@gmail.com>
2020-04-21 22:01:19 +0300

    [lab2][simplex] 1 test ok

--- lab2/src/simplex_method.py
+++ lab2/src/simplex_method.py
@@ -1,4 +1,5 @@
 import math
+from typing import Optional, Any
 
 import numpy as np
 
@@ -6,6 +7,7 @@ import numpy as np
 Simple simplex method from: 
     http://www.itlab.unn.ru/uploads/opt/optBook1.pdf
 """
+
 
 def gauss_step(A, r, s):
     n, m = A.shape
@@ -15,8 +17,10 @@ def gauss_step(A, r, s):
     for i in range(n):
         if r == i:
             continue
+        t = A[i, s]
         for j in range(m):
-            A[i, j] -= A[r, j] * A[i, s]
+            A[i, j] -= A[r, j] * t
+
 
 def core(A, B) -> bool:
     """
@@ -26,17 +30,17 @@ def core(A, B) -> bool:
     B - basis
     :return isSystem has bound solution
     """
-    # assert np.all(A[:, [0]] >= 0)
+    assert np.all(A[1:, [0]] >= 0)
 
     n, m = A.shape
     while True:
         s = None
         for j in range(1, n):
-            if A[0, j] > 0:
+            if A[0, j] < 0:
                 s = j
                 break
         if s is None:
-            return True
+            break
 
         r, t = None, math.inf  # TODO (Why min? Maybe use other way)
         for i in range(1, n):
@@ -49,10 +53,11 @@ def core(A, B) -> bool:
         # Gauss step
         gauss_step(A, r, s)
 
-        B[r-1] = s
+        B[r - 1] = s
+    return True
 
 
-def simplex_method(A, b, c):
+def simplex_method(A, b, c) -> Optional[Any]:
     """
     Presume:
     forall i, b[i] >= 0
@@ -67,29 +72,30 @@ def simplex_method(A, b, c):
     ## intros fake variables
     A = np.hstack((A, np.eye(len(A))))
 
-    ## intro new function (TODO: Why?)
+    ## intro new function
     """
     f = max(-x[n+1] - ... -x[n+m])
     """
-    # add top row (0,0,0,...-x[n+1], -x[n+2]... -x[n+m]) and left row (0, b[0], b[1]...b[n])
+    # add top row (0,0,0,...x[n+1], x[n+2]... x[n+m]) and left row (0, b[0], b[1]...b[n])
     z = np.concatenate((np.zeros(1), np.zeros(n), np.repeat(1, m)))
     Bz = np.arange(n + 1, n + m + 1)
     A = np.hstack((b.reshape((len(b), 1)), A))
     A = np.vstack((z.reshape((1, len(z))), A))
 
+    # get rid of 1 in fist row
     # row[0] -= sum(row[1..m])
-    for i in range(1, m+1):
-        A[0] = A[0] - A[i]
+    for i in range(1, m + 1):
+        A[0] = A[0] - A[i]  # equal gauss step for basic vector
 
-    # for r, i in enumerate(range(n+1, n+m+1), start=1):
-    #     gauss_step(A, r, i)
-
-
+    if not core(A, Bz):
+        return None
+    if A[0, 0] < 0:
+        return False
     # set invariant B_z
     for b_ind, s in enumerate(Bz):
         if n < s:
             r = None
-            for i in range(1, m+1):
+            for i in range(1, m + 1):
                 if r is None and A[i, s] == 1:
                     r = i
                 else:
@@ -97,18 +103,19 @@ def simplex_method(A, b, c):
             assert r is not None, "0-base vector"
 
             k = None
-            for i in range(1, n+1):
+            for i in range(1, n + 1):
                 if A[r, i] != 0:
                     k = i
                     break
             if k is None:
                 A = np.delete(A, r, 0)
+                Bz = np.delete(Bz, r - 1)
             else:
                 gauss_step(A, r, k)
                 Bz[b_ind] = k
 
     c = np.concatenate((np.zeros(1), -c))
-    A = np.vstack((c.reshape((1, len(c))), A[1:, 0:n+1]))
+    A = np.vstack((c.reshape((1, len(c))), A[1:, 0:n + 1]))
 
     for i, s in enumerate(Bz, start=1):
         if A[0, s] != 0:
@@ -116,15 +123,29 @@ def simplex_method(A, b, c):
             A[0] -= A[i] * A[0, s]
 
     first_opt_solution = np.zeros(n)
-    for s in Bz:
-        first_opt_solution[s - 1] = A[s, 0]
+    for i, s in enumerate(Bz, start=1):
+        first_opt_solution[s - 1] = A[i, 0]
 
     # second phase
+    if not core(A, Bz):
+        return None
+    solution = np.zeros(n)
+    for i, s in enumerate(Bz, start=1):
+        solution[s - 1] = A[i, 0]
+    res = A[0, 0]
+    return solution, res
 
-if __name__ == "__main__":
+
+def main():
     A = np.array([[1, 1, -1, 3],
                   [1, -2, 3, -1],
                   [5, -4, 7, 3]])
     b = np.array([1, 1, 5])
     c = np.array([-1, -1, 0, -5])
-    simplex_method(A, b, c)
+    x, res = simplex_method(A, b, c)
+    print(res)
+    print(x)
+
+
+if __name__ == "__main__":
+    main()
