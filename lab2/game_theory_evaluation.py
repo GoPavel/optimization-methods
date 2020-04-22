729430445f23c01e1a1df544546d33a57080f59f
Golovin Pavel <gopavel0@gmail.com>
2020-04-22 18:47:24 +0300

    [game-theory] add player 2

--- lab2/game_theory_evaluation.py
+++ lab2/game_theory_evaluation.py
@@ -3,18 +3,28 @@ import numpy as np
 from simplex_method import simplex_method
 
 
+def print_eval(A, b, c):
+    print(f'Sample:\nA:\n{A}\nb:{b}, c:{c}\n')
+    res, x = simplex_method(A, b, c, leq=False)
+    print(f'Maximum: {res} on {x}')
+
+    print(f'Result: {1 / res}\nx: {x / res} (sum: {np.sum(x / res)})')
+
+
 def main():
     A = np.array([[0, 1, 2, 3],
                   [1, 0, 1, 2],
                   [2, 1, 0, 1],
                   [3, 2, 1, 0]])
-    b = [1, 1, 1, 1]
-    c = [1, 1, 1, 1]
-    print(f'Sample:\nA:\n{A}\nb:{b}, c:{c}\n')
-    res, x = simplex_method(A, b, c, leq=False)
-    print(f'Minimum: {res} on {x}')
+    b = np.array([1, 1, 1, 1])
+    c = np.array([1, 1, 1, 1])
+    print('>>> First player <<<')
+    print_eval(A, b, c)
 
-    print(f'Result: {1 / res}\nx: {x / res}\nsum: {np.sum(x/res)}')
+    A = (-A + 3)
+    print('>>> Second player <<<')
+    print_eval(A, b, c)
+
 
 if __name__ == '__main__':
     main()
